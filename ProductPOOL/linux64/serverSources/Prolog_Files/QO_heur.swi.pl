6f61131241ed0e8df1c6cc4b1bc1f5901de1121e
Manfred Jeusfeld <manfred.jeusfeld@his.se>
2020-01-08 14:34:33 +0100

    synchronize

--- ProductPOOL/linux64/serverSources/Prolog_Files/QO_heur.swi.pl
+++ ProductPOOL/linux64/serverSources/Prolog_Files/QO_heur.swi.pl
@@ -1,7 +1,7 @@
 /**
 The ConceptBase.cc Copyright
 
-Copyright 1987-2019 The ConceptBase Team. All rights reserved.
+Copyright 1987-2020 The ConceptBase Team. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted
 provided that the following conditions are met:
