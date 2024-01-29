5c69a6bcf83a6f1a9ba63c06b618cbf39dd314b4
Manfred Jeusfeld <manfred.jeusfeld@his.se>
2024-01-29 11:46:33 +0100

    add showAnswer as a command that may have \ ; issue #64

--- ProductPOOL/linux64/serverSources/Prolog_Files/GlobalParameters.swi.pl
+++ ProductPOOL/linux64/serverSources/Prolog_Files/GlobalParameters.swi.pl
@@ -90,8 +90,8 @@ Legal home of the FreeBSD copyright license: http://www.freebsd.org/copyright/fr
 
 
 
-cb_version('8.2.22').
-cb_date_of_release('2023-07-25'). /* set via environment variable CB_DATE in startCBserver; see serverSources/Makefile */
+cb_version('8.2.23').
+cb_date_of_release('2024-01-28'). /* set via environment variable CB_DATE in startCBserver; see serverSources/Makefile */
 cb_location('Byskola').            /** place where CBserver is compiled, updated in startCBserver **/
 
 cb_copyright_time('Copyright 1987-2024').              /*keep up to date!*/
