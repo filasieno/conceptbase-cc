6cc4a18ba775357b257a53ca3b65749c6f6d5781
Manfred Jeusfeld <manfred.jeusfeld@his.se>
2026-06-04 16:57:49 +0200

    loadbalancer: shorter defailt wait time in startpoolservers.sh

--- ProductPOOL/examples/Clients/LoadBalancer/startpoolservers.sh
+++ ProductPOOL/examples/Clients/LoadBalancer/startpoolservers.sh
@@ -3,10 +3,10 @@
 # The pool cbservers are started in nonpersistent mode and use -g public, hence can serve multiple users in principle.
 # Each user gets a dedicated home directory such as System-oHome-maryA
 # We start here just 2 pool servers, each with its own log file. Add more as needed.
-# The restart delay is set to 20 seconds to mitigate denial of service
+# The restart delay is set to 2 seconds to mitigate denial of service
 
-(cbserver -port 5001 -r 20 -a $USER -g public -u nonpersistent -t minimal &>> log5001.log) &
-(cbserver -port 5002 -r 20 -a $USER -g public -u nonpersistent -t minimal &>> log5002.log) &
+(cbserver -port 5001 -r 2 -a $USER -g public -u nonpersistent -t minimal &>> log5001.log) &
+(cbserver -port 5002 -r 2 -a $USER -g public -u nonpersistent -t minimal &>> log5002.log) &
 
 
 
